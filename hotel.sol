3a26ca32c6eeb8f9f243a88c7fd5b39a4754ed4f
devansh7061 <83070984+devansh7061@users.noreply.github.com>
2023-01-26 18:50:45 +0530

    Update hotel.sol

--- hotel.sol
+++ hotel.sol
@@ -1,56 +1,82 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract HotelReservation{
-    
-    struct Room{
+contract HotelReservation {
+
+    /* state variables*/
+
+    int roomsAvailable;
+    int roomsBooked;
+    uint costOfOneRoom;
+
+    struct Room {
         bool reserved;
         address guest;
         int checkInDate;
     }
-    struct Guest{
+
+    struct Guest {
+        int roomNo;
+        string name;
+        int contactNo;
+        string email;
+        int noOfGuests;
         address guest;
-        int roomNum;
-    }
-    mapping(int => Room) public rooms;
-
-    int public roomsAvailable;
-    int public roomsBooked;
-    uint public roomPriceInEth;
-    constructor() {
-        roomsAvailable = 2;
-        roomsBooked = 0;
-        roomPriceInEth = 1;
-    }
-    event roomReserved(int roomNum, int checkInDate, address guest, uint price);
-    
-    function bookRoom(int _roomNum, int _checkIn) public payable {
-        require(roomsAvailable>0, "Sorry, all the rooms are booked");
-        require(msg.value>=roomPriceInEth, "U can't afford a room, kindly fuck off");
-        require(_roomNum>0 && _roomNum<=roomsAvailable, "Room doesn't exist");
-        require(rooms[_roomNum].reserved == false, "Room is already booked");
-        roomsAvailable-=1;
-        roomsBooked+=1;
-        rooms[_roomNum].reserved  = true;
-        rooms[_roomNum].guest  = msg.sender;
-        rooms[_roomNum].checkInDate  = _checkIn;
-        emit roomReserved(_roomNum, 5, msg.sender, msg.value);
     }
 
-    event bookingCancellation(int roomNum, address guest, uint refund);
+    constructor (int _roomsAvailable, int _roomsBooked, uint _costOfOneRoom) {
+        roomsAvailable = _roomsAvailable;
+        roomsBooked = _roomsBooked;
+        costOfOneRoom = _costOfOneRoom;
+    }
 
-    function cancelRoom(int _roomNum, int today) public{
-        require(rooms[_roomNum].reserved == true, "Nashe kmm krro");
+    mapping(int => Room) public Rooms;
+    mapping(address => Guest) public Guests;
+
+    event roomBooking(int checkInDate, uint price, int roomNo, address guest);
+    event roomCancelling(uint refund, int roomNo);
+
+    function bookRoom(int _roomNo, int _checkInDate, string memory _name, int _contactNo, string memory _email, int _noOfGuests) public payable {
+        require(roomsAvailable>0, "There are no rooms available");
+        require(_roomNo>0 && _roomNo<roomsAvailable, "Room doesn't exist");
+        require(msg.value >= costOfOneRoom, "Cost of one room is more than what you have paid");
+        require(Rooms[_roomNo].reserved == false, "Room is already booked");
+
+        roomsAvailable = roomsAvailable-1;
+        roomsBooked = roomsBooked+1;
+        Rooms[_roomNo].reserved = true;
+        Rooms[_roomNo].guest = msg.sender;
+        Rooms[_roomNo].checkInDate = _checkInDate;
+
+        Guests[msg.sender].name = _name;
+        Guests[msg.sender].noOfGuests = _noOfGuests;
+        Guests[msg.sender].email = _email;
+        Guests[msg.sender].contactNo = _contactNo;
+        Guests[msg.sender].roomNo = _roomNo;
+        emit roomBooking(_checkInDate, msg.value, _roomNo, msg.sender);
+    }
+
+    function cancelRoom(int _roomNo, int today) public {
+        require(Rooms[_roomNo].reserved == true, "There's no such booking");
+        require(Guests[msg.sender].roomNo == _roomNo, "U didn't book this room");
+        roomsAvailable = roomsAvailable+1;
+        roomsBooked = roomsBooked-1;
+        Rooms[_roomNo].reserved = false;
+        int checkInDate = Rooms[_roomNo].checkInDate;
         uint refund = 0;
-        roomsAvailable+=1;
-        roomsBooked-=1;
-        rooms[_roomNum].reserved = false;
-        if(rooms[_roomNum].checkInDate-today>=7){
-            refund = roomPriceInEth;
+        int timeForCheckIn = checkInDate-today;
+        if(timeForCheckIn<=2){
+            refund = 0;
         }
-        else if(rooms[_roomNum].checkInDate-today>=2){
-            refund = roomPriceInEth/2;
+        else if(timeForCheckIn>=7){
+            refund = costOfOneRoom;
         }
-        else refund = 0;
-        emit bookingCancellation(_roomNum, msg.sender, refund);
+        else {
+            refund = costOfOneRoom/2;
+        }
+        emit roomCancelling(refund, _roomNo);
     }
+
+
 }
